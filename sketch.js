954d0fd5a40a0874c4cac80ef8a2d542ee3997d4
galHabjan <95070076+crazycake919@users.noreply.github.com>
2023-10-22 02:32:24 +0200

    Revert "sa"
    This reverts commit 2bd653d6b511f9d9c4431aab93eb81f1542eb76f.

--- sketch.js
+++ sketch.js
@@ -1,16 +1,14 @@
 const frmRate = 30;
 
-let xScale = 1160; //1160
+let xScale = 1160;
 let yScale = 600;
 let number = 100;
-let maxNumber = 100;
 
-let numberScale = maxNumber / 10;
+let numberScale = number / 10;
 let xSize = xScale / number;
 let ySize = yScale + 60;
 let xOffSet = 60;
-let yOffSet = 50;
-let valueIncrease = (yScale / maxNumber);
+let valueIncrease = yScale / number;
 let arr = Array.from(Array(number), (_, i) => i + 1);
 
 // colors
@@ -22,22 +20,15 @@ let beep;
 let startTime = 0;
 let endTime = 0;
 let started = false;
-
-//algorithms
-let pickAlgorithm = 0;
-let pickShufle = 0;
-
-function initializeScene() {
-    number = parseInt($("#numbers").val());
-    maxNumber = parseInt($("#maxNumber").val());
-    numberScale = maxNumber / 10;
-    xSize = xScale / number;
+function initializeScene(n) {
+    numberScale = n / 10;
+    xSize = xScale / n;
     ySize = yScale + 60;
     xOffSet = 60;
-    valueIncrease = (yScale / maxNumber);
-    
+    valueIncrease = yScale / n;
+    arr = Array.from(Array(n), (_, i) => i + 1);
 
-    
+    setRandom();
     drawEverything();
 }
 function preload() {
@@ -47,11 +38,11 @@ function setup() {
     cnv = createCanvas(1280, 720);
     cnv.mouseClicked(start);
     frameRate(frmRate);
-
-
+    
+    setRandom();
     c1 = color("#0EDAF1"); // start
     c2 = color("#2418E7"); // end
-    initializeScene();
+    drawEverything();
     noLoop();
 
 }
@@ -60,123 +51,30 @@ function draw() {
 
 }
 
-async function start() {
+function start() {
 
-
+    
     if (!started) {
-
+    
         if (!isNaN($("#numbers").val()) && $("#numbers").val() == "") {
             return;
         }
-        if (!isNaN($("#maxNumber").val()) && $("#maxNumber").val() == "") {
-            return;
-        }
-
-        startTime = performance.now();
-
+        beep.loop();
         started = true;
+        number = parseInt($("#numbers").val());
+        initializeScene(number);
 
-        initializeScene();
+        //saveGif('BS.gif', 5);
+        //asyncBS();
+        asyncQS(0, number - 1);
 
-
-        switch (pickAlgorithm) {
-            case (0):
-                await asyncBS();
-                break;
-            case (1):
-                beep.loop();
-                await asyncQS(0, number - 1);
-                beep.pause();
-                break;
-            case (2):
-                await countSort();
-                break;
-            case (3):
-                await romanSort();
-                break;
-
-        }
-
-        endTime = performance.now();
-        let elapsed = endTime - startTime;
-        console.log(elapsed);
-        started = false;
-
-
-
-
-
-    }
-    //saveGif('BS.gif', 5);
-
-}
-async function countSort() {
-    let a = [].concat(arr);
-
-    arr = new Array(maxNumber + 1).fill(0) //c
-
-    //let c = new Array(number).fill(0);
-    let c;
-    for (let i = 0; i < number; i++) {
-        arr[a[i]]++;
-
-        await sleep(0);
-        drawEverything();
-    }
-    for (let i = 1; i <= maxNumber; i++) {
-        arr[i] += arr[i - 1];
-
-        await sleep(0);
-        drawEverything();
-    }
-
-    c = [].concat(arr);
-    arr = new Array(number).fill(0); //b
-    await sleep(0);
-    drawEverything();
-    console.log(a);
-    console.log(c);
-    for (let i = number - 1; i >= 0; i--) {
-        arr[--c[a[i]]] = a[i];
-        await sleep(0);
-        drawEverything();
     }
 
 
 }
-async function romanSort() {
-    let a = [].concat(arr);
-
-    arr = new Array(maxNumber + 1).fill(0) //c
-    //let c = new Array(number).fill(0);
-    let c;
-    for (let i = 0; i < number; i++) {
-        arr[a[i]]++;
-
-        await sleep(0);
-        drawEverything();
-    }
-
-    c = [].concat(arr);
-    arr = new Array(number).fill(0); //b
-    await sleep(0);
-    drawEverything();
-    let index = 0;
-    for (let i = 0; i <= maxNumber; i++) {
-        for (let j = 0; j < c[i]; j++) {
-            arr[index++] = i;
-            await sleep(0);
-            drawEverything();
-        }
-    }
-}
-async function regixSort() {
-
-}
-
 
 async function asyncBS() {
-
+    startTime = performance.now();
     for (let i = 0; i < number; i++) {
         for (let j = 0; j < number - 1; j++) {
             if (arr[j] > arr[j + 1]) {
@@ -190,15 +88,18 @@ async function asyncBS() {
         }
 
     }
-
+    endTime = performance.now();
+    let elapsed = endTime - startTime;
+    console.log(elapsed);
+    started = false;
 
 
 }
 async function asyncQS(begin, end) {
 
-
+    if (begin == 0 && end == number - 1)
+        startTime = performance.now();
     if (begin >= end) return;
-
     var mid;
     var w = arr[begin];
     var i = begin, j = end + 1;
@@ -248,7 +149,11 @@ async function asyncQS(begin, end) {
     // Record the end time and calculate elapsed time
     if (begin == 0 && end == number - 1) {
 
-
+        endTime = performance.now();
+        let elapsed = endTime - startTime;
+        console.log(elapsed);
+        started = false;
+        beep.pause();
     }
 
 
@@ -264,26 +169,26 @@ function sleep(millisecondsDuration) {
         setTimeout(resolve, millisecondsDuration);
     })
 }
-
+function drawNothing() {
+    background(100);
+}
 function drawEverything() {
-
+    
     background(0);
     lines();
     //rects
 
     stroke(0);
-    strokeWeight(2);
-
-    for (let i = 0; i < number; i++) {
-        drawSquare(xSize * i + xOffSet, ySize, arr[i]);
+    strokeWeight(1);
+     for (let i = 0; i < number; i++) {
+        drawSquare(xSize * i + xOffSet, ySize, valueIncrease * arr[i]);
     }
-    //text
-    stroke(255);
-    textSize(15);
-    strokeWeight(0.9);
-    textAlign(RIGHT);
 
-    for (let i = 0; i <= maxNumber; i++) {
+    //text
+    textSize(18);
+    textAlign(RIGHT);
+    
+    for (let i = 0; i <= number; i++) {
         if (i % numberScale == 0) {
             push();
             translate(50, yScale + 65 - valueIncrease * i);
@@ -292,16 +197,16 @@ function drawEverything() {
             pop();
         }
     }
-
+   
 }
 
 function lines() {
     //lines
     stroke(255);
     strokeWeight(1);
-    for (let i = 1; i <= maxNumber; i++) {
+    for (let i = 1; i <= number; i++) {
         if (i % numberScale == 0) {
-            line(xOffSet, yScale + 60 - valueIncrease * i, xOffSet + xScale, yScale + 60 - valueIncrease * i);
+            line(60, yScale + 60 - valueIncrease * i, 60 + xScale, yScale + 60 - valueIncrease * i);
         }
     }
     stroke(255);
@@ -309,55 +214,21 @@ function lines() {
     //x
     line(xOffSet - 1, ySize + 2, xOffSet + xScale + 5, ySize + 2);
     //y
-    line(xOffSet - 3, yOffSet, xOffSet - 3, ySize + 2);
+    line(xOffSet - 3, 55, xOffSet - 3, ySize + 2);
 }
 
 function drawSquare(x, y, value) {
     noFill();
-
-
-
     for (let i = x; i < x + xSize; i++) {
-
-        let inter = map(value, 1, maxNumber, 0, 1);
-        let smthing = 1.0 / ((maxNumber - 1) * 2)
-
-        let neki = map(i, x, x + xSize, -smthing, smthing);
+        let inter = map(value / valueIncrease, 1, number, 0, 1);
+        let neki = map(i, x, xSize, 0, 1 / number);
         inter += neki;
-
-        inter = map(inter, 0.0 - smthing, 1.0 + smthing, 0, 1);
-
         let c = lerpColor(c1, c2, inter);
         stroke(c);
-
-        line(i, y, i, y - value * valueIncrease);
-
-
+        line(i, y, i, y - value);
     }
 }
 
-function arrayShuffle() {
-
-    arr = shuffle(arr);
-    drawEverything();
-}
-function setIncremental() {
-    arr = new Array(number);
-    for (let i = 1; i <= number; i++) {
-        arr[i - 1] = Math.round(i * maxNumber / number);
-
-    }
-    drawEverything();
-}
 function setRandom() {
-    arr = new Array(number);
-    for (let i = 0; i < number; i++) {
-        arr[i] = Math.round(Math.random() * maxNumber);
-
-    }
-    drawEverything();
-}
-function setAlgorithm(n) {
-    pickAlgorithm = n;
-
+    arr = shuffle(arr);
 }
